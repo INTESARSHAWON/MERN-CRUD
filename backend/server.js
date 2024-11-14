1ea56892247079aec9c2e13d9bae6148fdb53c51
Intesar Shawon <intesarshawon@gmail.com>
2024-11-15 00:59:52 +0600

    MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
 
+dotenv.config();
 const app = express();
 
 app.get ("/products", (req, res) => {
-    res.send("server is ready");
+    // res.send("server is ready");
 })
 
+//console.log(process.env.MONGO_URI);
+
+
 app.listen(5000, () => {
+    connectDB();
     console.log('server started at http://localhost:5000');
-    
 })
+
+// 4tywOrOTZ8hqt5IN
