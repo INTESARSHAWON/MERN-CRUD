3cb7e5c06f6c70f7fd05d42ac069abb94b6d3ae2
Intesar Shawon <intesarshawon@gmail.com>
2024-11-16 00:26:48 +0600

    building API

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT | 5000;
 
-app.post ("/products", (req, res) => {
-    // res.send("server is ready");
-})
-
+app.use (express.json()); //allow us to accept JSON data in the req.body
+app.use ("/api/products/", productRoutes);
 //console.log(process.env.MONGO_URI);
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('server started at http://localhost:5000');
+    console.log('server started at http://localhost:' + PORT);
 })
 
 // 4tywOrOTZ8hqt5IN
